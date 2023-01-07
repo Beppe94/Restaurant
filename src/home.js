57eb801826b5e07d2ac5ce5d07d46edda2d634cf
Beppe <giuseppe.monorchio94@hotmail.it>
2023-01-07 18:21:28 +0100

    added proper function to display hours

--- src/home.js
+++ src/home.js
@@ -1,28 +1,49 @@
+function makeDays(day) {
+    const hoursParagraph = document.createElement('p')
+    hoursParagraph.innerText = day;
+
+    return hoursParagraph;
+}
+
 function createHome() {
     const infoDiv = document.createElement('div');
+    infoDiv.setAttribute('class', 'home-tab');
     infoDiv.setAttribute('id', 'home');
+    const hourDiv = document.createElement('div');
+    hourDiv.setAttribute('class', 'hours-class');
 
-    
     const introDiv = document.createElement('div');
+    introDiv.setAttribute('class', 'intro-class');
     const introTxt = document.createElement('h1');
-    introTxt.innerText = 'Intro';
+    introTxt.innerText = 'About Us';
     const paragraph = document.createElement('p');
     
     paragraph.innerText = 'Welcome to our pizzeria! We offer a wide variety of delicious pizzas made with fresh, high-quality ingredients. Our dough is made daily and hand-tossed to perfection, and our sauces are made from scratch using the finest tomatoes. In addition to pizza, we also have a selection of salads, pastas, and sandwiches to choose from. Come in and dine with us, or order online for pickup or delivery. We can\'t wait to serve you!';
+
+    const hourTxt = document.createElement('h1')
+    hourTxt.innerText = 'Hours';
+
     introDiv.appendChild(introTxt);
     introDiv.appendChild(paragraph);
-    
-    const hourDiv = document.createElement('div');
-    const hourTxt = document.createElement('h1');
-    hourTxt.innerText = 'Hours';
-    const hourParagraph = document.createElement('p');
-    
-    hourParagraph.innerText = 'Monday: 9am - 8pm Tuesday: 9am - 8pm Wednesday: 9am - 8pm Thursday: 9am - 8pm Friday: 9am - 9pm Saturday: 9am - 9pm Sunday: 9am - 9pm';
-    hourDiv.appendChild(hourTxt);
-    hourDiv.appendChild(hourParagraph);
-    
+
     infoDiv.appendChild(introDiv);
     infoDiv.appendChild(hourDiv);
+    hourDiv.appendChild(hourTxt);
+    
+    const days = [
+        makeDays('Monday: 9am - 8pm,'),
+        makeDays('Tuesday: 9am - 8pm,'),
+        makeDays('Wednesday: 9am - 8pm,'),
+        makeDays('Thursday: 9am - 8pm,'),
+        makeDays('Friday: 9am - 9pm,'),
+        makeDays('Saturday: 9am - 9pm,'),
+        makeDays('Sunday: 9am - 9pm'),
+    ]
+
+    days.forEach((day) => {
+        hourDiv.appendChild(day);
+    })
+    
     
     return infoDiv;
 }
